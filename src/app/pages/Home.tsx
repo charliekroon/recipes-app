a32ab9a3e4be34a6329f1c4b939463411b19dde8
Charlie Kroon <charlotkroon@protonmail.com>
2023-07-22 19:22:02 +0200

    first commit

---
+++ src/app/pages/Home.tsx
@@ -0,0 +1,8 @@
+// src/app/pages/Home.tsx
+import React from "react";
+
+const Home = () => {
+	return <div className="text-red-400 text-lg font-bold">RECIPES BY FABRI&CHARLIE</div>;
+};
+
+export default Home;
